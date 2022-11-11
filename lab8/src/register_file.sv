8472833062710a9285c9c8be7413f24361730c5d
João Carlos <joaocarlos@ecomp.uefs.br>
2022-11-11 15:21:19 -0300

    Corrige parâmetro - Quantidade de registradores.

--- lab8/src/register_file.sv
+++ lab8/src/register_file.sv
@@ -49,7 +49,7 @@ module register_file #(
 
    input wire clock,
    input wire wr,                            // WriteEnable:  se wr==1, o dado é escrito em mem
-   input wire [$clog2(Nloc)-1 : 0] ReadAddr1, ReadAddr2, WriteAddr, 	
+   input wire [$clog2(Nreg)-1 : 0] ReadAddr1, ReadAddr2, WriteAddr, 	
                                              // 3 endereóco, dois para leitura e um para escrita
    input wire [Dbits-1 : 0] WriteData,       // Dado a ser escrito na memória (se wr==1)
    output logic [Dbits-1 : 0] ReadData1, ReadData2
